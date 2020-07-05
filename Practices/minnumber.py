0aa87053b0686831618df1ce06dd3f7c43317b46
J Daniel R <52753172+daniel2483@users.noreply.github.com>
2020-07-05 10:22:23 -0600

    Add new script

---
+++ Practices/minnumber.py
@@ -0,0 +1,11 @@
+
+print("Finding the min number...")
+
+x=float(input("Enter 1st number: "))
+y=float(input("Enter 2nd number: "))
+z=float(input("Enter 3rd number: "))
+
+print("The min value is: ", min(x,y,z))
+
+
+input("Press enter to exit...")
