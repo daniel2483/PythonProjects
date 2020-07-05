97a3753bd792acf3013f8a2fa01381fd13fd99eb
J Daniel R <52753172+daniel2483@users.noreply.github.com>
2020-07-05 10:42:43 -0600

    Adding new script

---
+++ Practices/simple_calculator.py
@@ -0,0 +1,26 @@
+####################### Simple Caclulator
+#    Creator: Jose Daniel Rodriguez Sanchez
+
+def suma:
+    print ("Suma")
+
+def resta:
+    print ("Resta")
+    
+def div:
+    print ("Division")
+    
+def mult:
+    print ("Multiplicacin")
+
+########## Menu
+
+print ("\n\n")
+print ("##################################################")
+print ("#                                                #")
+print ("# Esto es una calculadora simple hecha en python #")
+print ("#                                                #")
+print ("##################################################")
+
+print ("\n\n\tOpciones:")
+
