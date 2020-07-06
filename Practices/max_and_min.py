8acfd8b5bfbd1e80157883f6b595f4e2dd0ed18e
J Daniel R <52753172+daniel2483@users.noreply.github.com>
2020-07-06 13:25:30 -0600

    Create New script

---
+++ Practices/max_and_min.py
@@ -0,0 +1,16 @@
+
+print("Finding the max number and min number...")
+
+x=float(input("Enter 1st number: "))
+y=float(input("Enter 2nd number: "))
+z=float(input("Enter 3rd number: "))
+w=float(input("Enter 4th number: "))
+
+print("\nResults:")
+print("The max value is: ", max(x,y,z,w))
+print("The min value is: ", min(x,y,z,w))
+print("The Summatory is: ", str((float(x)+float(y)+float(z)+float(w))))
+print("The Average is: ", str((float(x)+float(y)+float(z)+float(w))/4))
+
+
+input("Press enter to exit...")
