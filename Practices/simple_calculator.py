28ff7c9a352ba3b5841e26bbb09799eceb308537
J Daniel R <52753172+daniel2483@users.noreply.github.com>
2020-07-07 11:26:54 -0600

    Update Simple calculator

--- Practices/simple_calculator.py
+++ Practices/simple_calculator.py
@@ -18,13 +18,15 @@ def menu():
     print ("\t2. Presiona esta opcion para restar dos numeros.")
     print ("\t3. Presiona esta opcion para multiplicar dos numeros.")
     print ("\t4. Presiona esta opcion para dividir dos numeros.")
-    print ("\t5. Presiona esta opcion para salir del programa.")
+    print ("\t5. Presiona esta opcion para dsacar la potencia de valor1^valor2.")
+    print ("\t6. Presiona esta opcion para salir del programa.")
 
     print ("\n\n\tSelecciona una opcion de arriba...")
     opcion = input ("\n\tIngresa la operacion: ")
 
     # Salir
-    if (str(opcion) == "5"):
+    if (str(opcion) == "6"):
+        salir()
         sys.exit()
 
     num1 = input ("\tEl valor 1: ")
@@ -49,6 +51,10 @@ def operacion(opcion,num1,num2):
     # Dividiendo
     elif (str(opcion) == "4"):
         div(num1,num2)
+        
+    # Potencia
+    elif (str(opcion) == "5"):
+        pot(num1,num2)
         
 
     # Mensaje Error
@@ -86,9 +92,16 @@ def mult(number1,number2):
     input("\n\tPresione enter para realizar otra operacion")
     opcion,num1,num2 = menu()
     operacion(opcion,num1,num2)
+
+def pot(number1,number2):
+    mul_result = pow(float(number1),float(number2))
+    print ("\n\tPotencia: " + str(number1) + " ^ " + str(number2) + " = " + str(mul_result))
+    input("\n\tPresione enter para realizar otra operacion")
+    opcion,num1,num2 = menu()
+    operacion(opcion,num1,num2)
     
 def salir():
-    print ("Saliendo de la aplicacion")
+    print ("\n\tSaliendo de la aplicacion")
     
 
 
@@ -97,9 +110,3 @@ def salir():
 
 opcion,num1,num2 = menu()
 operacion(opcion,num1,num2)
-
-
-
-
-
-
