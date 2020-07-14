19f70f829d6aaf2112135455335476103468e547
J Daniel R <52753172+daniel2483@users.noreply.github.com>
2020-07-14 12:18:57 -0600

    Script Examples
    This is how a module is created in python

---
+++ Practices/first_module.py
@@ -0,0 +1,6 @@
+# This is a module examples
+
+def hello_printer():
+    print("Hello WORLD!")
+
+name = "Jose Daniel Rodriguez"
---
+++ Practices/using_create_module.py
@@ -0,0 +1,5 @@
+import first_module
+
+first_module.hello_printer()
+
+print("This is my first module created by: " + first_module.name)
