e6700132ce1e831199ed5075d3749462dd90b4c4
J Daniel R <52753172+daniel2483@users.noreply.github.com>
2020-07-16 23:53:40 -0600

    Adding new scripts
    - Update email_checker_and_domain_result.py which have 2 functions: 1 return the domain of an email and the errors (True or False)
    - New Test Script send_email_to.py which ask for an email as an input and use the previous script to check if email is correct or not

--- Practices/email_checker_and_domain_result.py
+++ Practices/email_checker_and_domain_result.py
@@ -4,47 +4,52 @@ def domain_name (email):
 
     domain = ""
     error = False
-    
+
     try :
         email_length = email.split('@')
         user,domain = email.split('@')
-        
+
         test_email = len(email_length)
-        
+
         if (test_email != 2 or domain == ""):
             error = True
             #print ("This is not a correct email address...")
             #sys.exit()
-    
-        print ("Email: " + email + " | Domain: " + domain)
+
+        #print ("Email: " + email + " | Domain: " + domain)
     except ValueError :
         error = True
         #print ("This is not a correct email address...")
         #sys.exit()
-    
-    
+
+
     try :
         domain_length = domain.split('.')
         domain_name,domain_register =  domain.split('.')
         test_domain = len(domain_length)
-    
+
         if (test_domain != 2 or domain_register == ""):
             error = True
             #print ("This is not a correct domain for email address...")
             #sys.exit()
-            
-        print ("Domain Name: " + domain_name + " | Domain Register: " + domain_register)
+
+        #print ("Domain Name: " + domain_name + " | Domain Register: " + domain_register)
     except ValueError :
         error = True
         #print ("This is not a correct domain for email address...")
         #sys.exit()
-    
+
     return domain,error
-    
-    
-    
-email = input ("Please enter your email address: " )
 
-domain,error = domain_name(email)
+def email_address(email):
+        domain,error = domain_name(email)
+        print ("Dominio de correo: " + domain + " | Error: " + str(error))
+        return domain,error
+        
+        
 
-print ("This is the domain: " + domain + " | The domain is not correct: " + str(error)) 
+#email = input ("Please enter your email address: " )
+
+#domain,error = domain_name(email)
+
+#print ("This is the domain: " + domain + " | The domain is not correct: " + str(error))
---
+++ Practices/send_email_to.py
@@ -0,0 +1,7 @@
+import email_checker_and_domain_result as email_check
+
+
+
+email = input ("Email address: ")
+
+email_check.email_address(email)
