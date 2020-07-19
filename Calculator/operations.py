1288affb9dc1b6a5c8a4152e9f1c8b21b0cb4ce7
J Daniel R <52753172+daniel2483@users.noreply.github.com>
2020-07-19 07:16:01 -0600

    Update Script
    - Fixing Errors
    - Fixing Divided by Zero

--- Calculator/operations.py
+++ Calculator/operations.py
@@ -2,6 +2,7 @@ import math
 
 def sum(number1,number2):
     sum_result = float(number1) + float(number2)
+    #print (str(number1) + " + " + str(number2) + " = " + str(sum_result))
     return sum_result
 
 def sub(number1,number2):
@@ -9,7 +10,10 @@ def sub(number1,number2):
     return res_result
     
 def div(number1,number2):
-    div_result = float(number1) / float(number2)
+    try:
+        div_result = float(number1) / float(number2)
+    except ZeroDivisionError:
+        div_result = "∞"
     return div_result
     
 def mult(number1,number2):
@@ -26,7 +30,7 @@ def floor(number1,number2):
 
 def pot(number1,number2):
     pow_result = pow(float(number1),float(number2))
-    return mul_result
+    return pow_result
 
 def fac(number1):
     fac_result = math.factorial(int(number1))
