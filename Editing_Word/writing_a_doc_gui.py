90b1095a79872753b98a5ff07c13664d1b237e0b
J Daniel R <52753172+daniel2483@users.noreply.github.com>
2020-08-03 18:32:26 -0600

    Updating Script
    - Adding new fields in GUI

--- Editing_Word/writing_a_doc_gui.py
+++ Editing_Word/writing_a_doc_gui.py
@@ -13,11 +13,11 @@ from wx.lib.masked import NumCtrl
 class MyFrame(wx.Frame):
 
     def __init__(self):
-        super().__init__(parent=None, title='Word Letter Creator',size=(550, 500))
+        super().__init__(parent=None, title='Word Letter Creator',size=(750, 620))
         panel = wx.Panel(self)
 
         #self.wx.Frame.
-
+        # pos=(x,y)
 
         pos_ini_y = 10
         pos_ini_x = 150
@@ -27,17 +27,30 @@ class MyFrame(wx.Frame):
         self.sucursal = wx.ComboBox(panel,id = 1 , value = "", pos=(pos_ini_x, pos_ini_y), size=(250, 20), choices = sucursales, style = wx.CB_DROPDOWN)
 
         wx.StaticText(panel, label="Carta dirigida a: ", pos=(pos_ini_x - 100, pos_ini_y + 30))
-        self.sendto = wx.TextCtrl(panel, pos=(pos_ini_x, pos_ini_y + 30), size=(160, 20),value = "", style=wx.TE_LEFT)
-        # pos=(x,y)
+        self.sendto = wx.TextCtrl(panel, pos=(pos_ini_x, pos_ini_y + 30), size=(250, 20),value = "", style=wx.TE_LEFT)
 
         wx.StaticText(panel, label="Departamento: ", pos=(pos_ini_x - 100, pos_ini_y + 60))
-        self.department = wx.TextCtrl(panel, pos=(pos_ini_x, pos_ini_y + 60), size=(160, 20),value = "", style=wx.TE_LEFT)
+        self.department = wx.TextCtrl(panel, pos=(pos_ini_x, pos_ini_y + 60), size=(250, 20),value = "", style=wx.TE_LEFT)
+
+        wx.StaticText(panel, label="Saludo Inicial: ", pos=(pos_ini_x - 100, pos_ini_y + 90))
+        self.greetings = wx.TextCtrl(panel, pos=(pos_ini_x, pos_ini_y + 90), size=(250, 20),value = "", style=wx.TE_LEFT)
+
+        wx.StaticText(panel, label="Razón de la carta: ", pos=(pos_ini_x - 100, pos_ini_y + 120))
+        self.reason = wx.TextCtrl(panel, pos=(pos_ini_x, pos_ini_y + 120), size=(520, 200),value = "", style=wx.TE_LEFT | wx.TE_MULTILINE)
+
+        wx.StaticText(panel, label="Nombre quien firma: ", pos=(pos_ini_x - 120, pos_ini_y + 330))
+        self.signature = wx.TextCtrl(panel, pos=(pos_ini_x, pos_ini_y + 330), size=(250, 20),value = "", style=wx.TE_LEFT)
 
         # First Status disable when no sucursal is selected
         self.sendto.Disable()
         self.department.Disable()
+        self.greetings.Disable()
+        self.reason.Disable()
+        self.signature.Disable()
 
-        self.my_btn_save = wx.Button(panel, label='Guardar', pos=(pos_ini_x-50, pos_ini_y + 200), size=(100, 40))
+
+        self.my_btn_save = wx.Button(panel, label='Guardar', pos=(pos_ini_x-50, pos_ini_y + 450), size=(100, 40))
+        self.my_btn_save.Disable()
 
         # Results
         self.operation = wx.StaticText(panel, label="", pos=(pos_ini_x + 175, pos_ini_y))
@@ -53,9 +66,9 @@ class MyFrame(wx.Frame):
         fontButton = wx.Font(11, wx.DECORATIVE, wx.NORMAL, wx.BOLD)
 
 
-        wx.StaticText(panel, label="Made by: Jose Daniel Rodríguez Sánchez", pos=(pos_ini_x, pos_ini_y + 325))
-        wx.StaticText(panel, label="Build on: 2020-08-01", pos=(pos_ini_x, pos_ini_y + 345))
-        wx.StaticText(panel, label="Last Update: 2020-08-03", pos=(pos_ini_x, pos_ini_y + 365))
+        wx.StaticText(panel, label="Made by: Jose Daniel Rodríguez Sánchez", pos=(pos_ini_x, pos_ini_y + 500))
+        wx.StaticText(panel, label="Build on: 2020-08-01", pos=(pos_ini_x, pos_ini_y + 520))
+        wx.StaticText(panel, label="Last Update: 2020-08-03", pos=(pos_ini_x, pos_ini_y + 540))
 
         ### Set event handlers
         # Event for combo sucursal
@@ -90,22 +103,18 @@ class MyFrame(wx.Frame):
         self.my_btn_clear_all.Refresh()
 
     def OnButtonSave(self, e):
-        value = self.sendto.GetValue()
-        if (value != 0):
-            value = str(value) + "1"
-            print (value)
-            if re.search("^01", value):
-                value_len = len(value)
-                value = value[1:value_len]
-            #value = int(value)
-            self.sendto.SetLabel(value)
+        print ("Saving File ...")
 
     def OnCombo(self, event):
         #self.label.SetLabel("selected "+ self.combo.GetValue() +" from Combobox")
         negocio = self.sucursal.GetValue()
-        print (negocio)
+        #print (negocio)
+        self.my_btn_save.Enable()
         self.sendto.Enable()
         self.department.Enable()
+        self.greetings.Enable()
+        self.reason.Enable()
+        self.signature.Enable()
 
 if __name__ == '__main__':
     app = wx.App()
