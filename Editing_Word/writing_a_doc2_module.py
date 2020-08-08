91db2bf5ca08f77b4bd4da50d66f59627ed8580f
J Daniel R <52753172+daniel2483@users.noreply.github.com>
2020-08-07 23:29:56 -0600

    New module to create letter

---
+++ Editing_Word/writing_a_doc2_module.py
@@ -0,0 +1,42 @@
+# pip install docx-mailmerge
+
+import docx
+
+def saving_word_letter(business,send_to_txt,department_txt,greetings,reason_to_contact,person_name_txt,id_number_txt,path_file):
+    #send_to_txt = input ("\nPara: ")
+    #department_txt = input ("Departamento: ")
+    #greetings = input ("Saludos: ")
+    #reason_to_contact = input ("Razon de la carta: ")
+    #person_name_txt = input ("Nombre de la persona que solicita: ")
+    #id_number_txt = input ("Número de Cédula: ")
+
+    path = path_file
+
+    #print (str(business))
+
+    if ( business != 0):
+        template = "carta_generica_RS.docx"
+    else:
+        template = "carta_generica_NI.docx"
+
+    document = MailMerge( path + template)
+
+# To check the merge fields on word Template
+#print(document.get_merge_fields())
+
+    document.merge(
+        current_date='{:%d-%m-%Y}'.format(date.today()),
+        send_to=send_to_txt,
+        department=department_txt,
+        greetings=greetings,
+        reason_to_contact=reason_to_contact,
+        person_name=person_name_txt,
+        id_number=id_number_txt)
+
+    document.write(path + 'carta_final.docx')
+
+current_dir = os.getcwd()
+path_file= os.path.expanduser(current_dir + "/Templates/")
+template = "carta_generica_RS.docx"
+doc = docx.Document()
+
