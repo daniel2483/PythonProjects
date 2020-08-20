66fc21b2644c672f731c23bfd0515b9b8c1d996d
J Daniel R <52753172+daniel2483@users.noreply.github.com>
2020-08-20 10:43:27 -0600

    Create new exercise

---
+++ Exercises/exercise_intro_python_2.py
@@ -0,0 +1,10 @@
+minute = 60
+hour = 60
+minute * hour
+
+seconds_per_hour = minute * hour
+24 * seconds_per_hour
+
+seconds_per_day = 24 * seconds_per_hour
+seconds_per_day / seconds_per_hour
+seconds_per_day // seconds_per_hour
