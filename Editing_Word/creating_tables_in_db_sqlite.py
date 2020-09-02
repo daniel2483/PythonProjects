96b7fb0e6d37474e39dd75346d94513a16a9a26b
J Daniel R <52753172+daniel2483@users.noreply.github.com>
2020-09-01 18:07:40 -0600

    New scripts

---
+++ Editing_Word/creating_tables_in_db_sqlite.py
@@ -0,0 +1,62 @@
+import os
+import sqlite3
+from sqlite3 import Error
+
+current_dir = os.getcwd()
+print ("Current Directory: " + current_dir)
+
+db_path = current_dir + "\db\pythonsqlite.db"
+print ("Current DB Path: " + db_path)
+
+# Create DB Connection
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return conn
+
+# Create Tables using connection and SQL Statement Table Creation
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+def main():
+    database = db_path
+
+    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        begin_date text,
+                                        end_date text
+                                    ); """
+
+
+    # create a database connection
+    conn = create_connection(database)
+
+    # create tables
+    if conn is not None:
+        # create projects table
+        create_table(conn, sql_create_projects_table)
+
+        # create tasks table
+        create_table(conn, sql_create_tasks_table)
+    else:
+        print("Error! cannot create the database connection.")
