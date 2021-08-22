587c5f0051f556de1998c16aa50254e98eaa291b
Jose Daniel Rodriguez Sanchez <jdrs2483@gmail.com>
2021-08-22 14:19:53 -0600

    Python exercises

--- Examples/drawing_diamond.py
+++ Examples/drawing_diamond.py
@@ -4,10 +4,10 @@ def check_if_pair(number):
     test = number%2
     if test == 0:
         center = test
-        return True
+        return center
     else:
         center = math.ceil(test)
-        return False
+        return center
 
 
 height = int(input("Enter the height of rectangule (must be an integer): "))
@@ -15,11 +15,11 @@ width = int(input("Enter the width of rectangule (must be an integer): "))
 
 counter_heigh = 0
 
-check_height_pair = check_if_pair(height)
-check_width_pair = check_if_pair(width)
+center = check_if_pair(height)
+center = check_if_pair(width)
 
-string_first_line = ""
-string_other_line = ""
+string_lines = ""
+#string_other_line = ""
 
 counter_line = 0
 
@@ -29,15 +29,15 @@ for height_dimension in range(0,height):
             string_first_line = string_first_line + "*"
             if width_dimention == (width-1):
                 print(string_first_line)
-    elif (counter_heigh < (height-1)):
-        if (counter_line == 0):
-            for width_dimention in range(0,width):
-                if width_dimention == 0 or width_dimention == (width-1):
-                    string_other_line = string_other_line + "*"
-                else:
-                    string_other_line = string_other_line + " "
-        print(string_other_line)
-        counter_line = counter_line + 1
+    #elif (counter_heigh < (height-1)):
+    #    if (counter_line == 0):
+    #       for width_dimention in range(0,width):
+    #            if width_dimention == 0 or width_dimention == (width-1):
+    #                string_other_line = string_other_line + "*"
+    #            else:
+    #                string_other_line = string_other_line + " "
+    #    print(string_other_line)
+    #    counter_line = counter_line + 1
     else:
         print(string_first_line)
     counter_heigh = counter_heigh + 1
