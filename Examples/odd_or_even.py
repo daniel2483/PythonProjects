9859b82c7f05fe8ce7b1a8b173eb14128d447010
Jose Daniel Rodriguez Sanchez <jdrs2483@gmail.com>
2021-08-26 14:27:17 -0600

    Python exercises

--- Examples/odd_or_even.py
+++ Examples/odd_or_even.py
@@ -15,3 +15,32 @@
 # 2. Ask the user for two numbers: one number to check (call it num) and one number to 
 # divide by (check). If check divides evenly into num, tell that to the user. If not, print 
 # a different appropriate message.
+
+loop = 0
+
+while loop == 0:
+    try:
+        number = int(input("Please enter a number to see if is even or odd: "))
+        loop = 1
+    except ValueError:
+        loop = 0
+        print("Value is not an integer")
+
+if number%2 == 1:
+    print ("This number: "+str(number)+" is an odd")
+if number%2 == 0:
+    if number%4 == 0:
+        print ("This number: "+str(number)+" is an even and can be divided by four.")
+    else:
+        print ("This number: "+str(number)+" is an even and can't be divided by four.")
+
+loop = 0
+while loop == 0:
+    try:
+        num = int(input("Please enter an integer number: "))
+        check = int(input("Please enter an integer number to do a division: "))
+        
+        loop = 1
+    except ValueError:
+        loop = 0
+        print("Value is not an integer")
